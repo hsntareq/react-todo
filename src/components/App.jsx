a6e9d217dfefc1b63adf24be847728033c5a2663
Hasan Tareq <hsntareq@gmail.com>
2023-02-26 19:19:04 +0600

    adding items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,108 @@
+import { useState } from 'react';
 import '../App.scss';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Title one',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Title two',
+      isComplete: true,
+    },
+    {
+      id: 3,
+      title: 'Title three',
+      isComplete: true,
+    },
+  ]);
+
+  const [todoInput, setTodoInput] = useState('');
+  const [idTodo, setIdTodo] = useState('');
+
+  const addTodo = e => {
+    e.preventDefault();
+
+    if (todoInput.trim().length === 0) return;
+
+    setTodos([
+      ...todos,
+      {
+        id: idTodo,
+        title: todoInput,
+        isComplete: false,
+      },
+    ]);
+
+    setTodoInput('');
+    setIdTodo(prevIdTodo => prevIdTodo + 1);
+  };
+
+  const handleInput = e => {
+    setTodoInput(e.target.value);
+  };
+  const deleteTodo = id => {
+    setTodos([...todos].filter(todo => todo.id !== id));
+  };
+  const completeTodo = id => {
+    setTodos([...todos].filter(todo => todo.id !== id));
+    const updatedTodo = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodo);
+  };
+
   return (
-    <div className="App shadow-lg p-5 mt-20 border rounded">
+    <div className="App shadow-lg p-5 my-10 border rounded">
       <div className="todo_app">
-        <h1 className="mb-3">Todo App</h1>
-        <input
-          className="border-b w-full py-2 px-3 focus:outline-none focus:ring-1 focus:ring-gray-100 ring-inset"
-          type="text"
-          placeholder="What do you need to do?"
-        />
+        <h1 className="mb-3">Todo Application</h1>
+
+        <form action="" onSubmit={addTodo}>
+          <input
+            className="border-b w-full py-2 px-3 focus:outline-none focus:ring-1 focus:ring-gray-100 ring-inset"
+            type="text"
+            value={todoInput}
+            onChange={handleInput}
+            placeholder="What do you need to do?"
+          />
+        </form>
+
         <ul className="divide-y divide-slate-200">
-          <li className="flex flex-row justify-between items-center">
-            <input className="cursor-pointer" type="checkbox" id="it1" />
-            <label className="cursor-pointer flex-1 p-3" htmlFor="it1">
-              Finish React series
-            </label>
-            <button className="button p-2">✕</button>
-          </li>
-          <li className="flex flex-row justify-between items-center">
-            <input className="cursor-pointer" type="checkbox" id="it2" />
-            <label className="cursor-pointer flex-1 p-3" htmlFor="it2">
-              Finish React series
-            </label>
-            <button className="button p-2">✕</button>
-          </li>
-          <li className="flex flex-row justify-between items-center">
-            <input className="cursor-pointer" type="checkbox" id="it3" />
-            <label className="cursor-pointer flex-1 p-3" htmlFor="it3">
-              Finish React series
-            </label>
-            <button className="button p-2">✕</button>
-          </li>
-          <li className="flex flex-row justify-between items-center">
-            <input className="cursor-pointer" type="checkbox" id="it4" />
-            <label className="cursor-pointer flex-1 p-3" htmlFor="it4">
-              Finish React series
-            </label>
-            <button className="button p-2">✕</button>
-          </li>
-          <li className="flex flex-row justify-between items-center">
-            <input className="cursor-pointer" type="checkbox" id="it5" />
-            <label className="cursor-pointer flex-1 p-3" htmlFor="it5">
-              Finish React series
-            </label>
-            <button className="button p-2">✕</button>
-          </li>
+          {todos.map((todo, index) => (
+            <li
+              key={todo.id}
+              className="flex flex-row justify-between items-center"
+            >
+              <input
+                className="cursor-pointer"
+                type="checkbox"
+                id={`it${todo.id}`}
+                onChange={() => completeTodo(todo.id)}
+                checked={todo.isComplete ? true : false}
+              />
+              <label
+                className={`cursor-pointer flex-1 p-3 ${
+                  todo.isComplete && 'line-through'
+                }`}
+                htmlFor={`it${todo.id}`}
+              >
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="button p-2"
+              >
+                ✕
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="border-t border-b flex justify-between py-3 items-center">
